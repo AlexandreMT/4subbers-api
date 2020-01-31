d46e61d8b08af9e032cc48417097fc147aae4e46
Alexandre Moraes Matos <alexandremt03@gmail.com>
2020-01-31 10:56:45 -0300

    cors

--- routes/api.php
+++ routes/api.php
@@ -3,22 +3,26 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 
-Route::prefix('subtitle-split')->group(function () {
-    Route::post('/cue', 'SplitByCue@SplitByCue');
 
-    Route::post('/time', 'SplitByTime@splitByTime');
 
-    Route::get('/get-parts/{url}', 'SplitByCue@getParts');
+Route::group(['middleware' => ['cors']], function () {
+    Route::prefix('subtitle-split')->group(function () {
+        Route::post('/cue', 'SplitByCue@SplitByCue');
 
-    Route::get('/storage/{filename}', function ($filename) {
-        if (!Storage::exists('4subbers/' . $filename)) {
-            abort(404);
-        }
+        Route::post('/time', 'SplitByTime@splitByTime');
 
-        return Storage::download('4subbers/' . $filename);
+        Route::get('/get-parts/{url}', 'SplitByCue@getParts');
+
+        Route::get('/storage/{filename}', function ($filename) {
+            if (!Storage::exists('4subbers/' . $filename)) {
+                abort(404);
+            }
+
+            return Storage::download('4subbers/' . $filename);
+        });
     });
-});
 
-Route::prefix('subtitle-statistics')->group(function () {
-    Route::post('/statistics', 'SubtitleStatistics@subtitleStatistics');
+    Route::prefix('subtitle-statistics')->group(function () {
+        Route::post('/statistics', 'SubtitleStatistics@subtitleStatistics');
+    });
 });
