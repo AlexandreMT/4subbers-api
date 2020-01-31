31edf9e5098354ab77f24487a9465c89e989f40b
Alexandre Moraes Matos <alexandremt03@gmail.com>
2020-01-31 11:15:41 -0300

    change cors middleware

--- routes/api.php
+++ routes/api.php
@@ -5,24 +5,22 @@ use Illuminate\Support\Facades\Storage;
 
 
 
-Route::group(['middleware' => ['cors']], function () {
-    Route::prefix('subtitle-split')->group(function () {
-        Route::post('/cue', 'SplitByCue@SplitByCue');
+Route::prefix('subtitle-split')->group(function () {
+  Route::post('/cue', 'SplitByCue@SplitByCue');
 
-        Route::post('/time', 'SplitByTime@splitByTime');
+  Route::post('/time', 'SplitByTime@splitByTime');
 
-        Route::get('/get-parts/{url}', 'SplitByCue@getParts');
+  Route::get('/get-parts/{url}', 'SplitByCue@getParts');
 
-        Route::get('/storage/{filename}', function ($filename) {
-            if (!Storage::exists('4subbers/' . $filename)) {
-                abort(404);
-            }
+  Route::get('/storage/{filename}', function ($filename) {
+      if (!Storage::exists('4subbers/' . $filename)) {
+          abort(404);
+      }
 
-            return Storage::download('4subbers/' . $filename);
-        });
-    });
-
-    Route::prefix('subtitle-statistics')->group(function () {
-        Route::post('/statistics', 'SubtitleStatistics@subtitleStatistics');
-    });
+      return Storage::download('4subbers/' . $filename);
+  });
 });
+
+Route::prefix('subtitle-statistics')->group(function () {
+  Route::post('/statistics', 'SubtitleStatistics@subtitleStatistics');
+})->middleware('cors');
