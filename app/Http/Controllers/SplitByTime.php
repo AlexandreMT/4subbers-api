00658212bcebcfa28f4ca7b37accf1750be57a08
Alexandre Moraes Matos <alexandremt03@gmail.com>
2020-02-03 15:47:12 -0300

    commit test

--- app/Http/Controllers/SplitByTime.php
+++ app/Http/Controllers/SplitByTime.php
@@ -18,6 +18,7 @@ class SplitByTime extends Controller
 
     public function splitByTime(Request $request) {
         try {
+            $request->replace($request->all());
             $projectName = $request->get('name');
             $subtitle = new SubripFile($request->file('subtitle'));
             $parts = $request->get('parts');
